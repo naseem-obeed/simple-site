bc2b45391602eef638947a0864a94d0f72150d0c
naseem-obeed <117030707+naseem-obeed@users.noreply.github.com>
2022-11-04 23:59:10 +0300

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,15 @@
+
+// let courses = document.querySelectorAll('.carousel-cards .carousel-item');
+
+// courses.forEach((el) => {
+//     const minPerSlide = 4
+//     let next = el.nextElementSibling
+//     for (var i = 1; i < minPerSlide; i++) {
+//        if(!next){
+//         next = courses[0]
+//     }
+//     let cloneChild = next.cloneNode( true)
+//     el.appendchild( cloneChild.children[0])
+//     next = next.nextElementSibling
+//     }
+// })
